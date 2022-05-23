44ceb94323fac64e8f62f247c312d76067ec878c
Johnexzy <obajohn75@gmail.com>
2022-05-23 22:38:56 +0100

    :alembic: (added postgre)

--- src/core/sequelize.ts
+++ src/core/sequelize.ts
@@ -1,11 +1,10 @@
 import { Sequelize } from "sequelize";
 import { WalletModel, walletCreation } from "../models/Wallet";
 import { TokenModel, tokenCreation } from "../models/Token";
-const sequelize: Sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "../db/web3gg.sqlite",
-});
+const sequelize = new Sequelize(
+  "postgres://hyccnrkpeozskc:3c3f75a82f323208315e3c4bb7ba330a94f07faf8032d5afdb2178cbf99a5588@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d8ja28r2gmut18"
+);
 
 WalletModel.init(walletCreation, { tableName: "Wallets", sequelize });
 TokenModel.init(tokenCreation, { tableName: "Tokens", sequelize });
-export default sequelize
+export default sequelize;
