7eb0faa26d0406f2f5dd081b4e1265fd16ab26a6
Johnexzy <obajohn75@gmail.com>
2022-05-24 12:16:25 +0100

    :rocket: (deploying to mainnet.) Added production confi…

--- src/core/sequelize.ts
+++ src/core/sequelize.ts
@@ -1,16 +1,9 @@
 import { Sequelize } from "sequelize";
 import { WalletModel, walletCreation } from "../models/Wallet";
 import { TokenModel, tokenCreation } from "../models/Token";
-const sequelize = new Sequelize(process.env.PG_DB!,  process.env.PG_USER!, process.env.PG_PASS!,{
-  host: process.env.PG_HOST,
-  port: parseInt(process.env.PG_PORT!),
-  dialect: "postgres",
-  dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
-      }
-   },
+
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
 WalletModel.init(walletCreation, { tableName: "Wallets", sequelize });
 TokenModel.init(tokenCreation, { tableName: "Tokens", sequelize });
