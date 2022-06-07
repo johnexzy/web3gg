9b924b35f6709d8c54a22e6786fcf73e48666d4e
Johnexzy <obajohn75@gmail.com>
2022-06-07 16:38:33 +0100

    :lipstick: (added numbers to commands)

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -13,12 +13,19 @@ export const Commands: ICommand = {
       const embed = new MessageEmbed()
         .setTitle("Web3Bot")
         .setDescription("All registered slash commands\n\n")
-        .setAuthor({ name: 'web3bot', iconURL: 'https://i.imgur.com/jP0MDWk.png', url: 'https://web3bot.gg' })
-        
+        .setAuthor({
+          name: "web3bot",
+          iconURL: "https://i.imgur.com/jP0MDWk.png",
+          url: "https://web3bot.gg",
+        })
+
         .addFields(
-          ...CommandList.map((command) => ({
-            name: command.data.description,
-            value: "\u200b \u200b"+inlineCode(`/${command.data.name}`)+"\n\n\n\n",
+          ...CommandList.map((command, i) => ({
+            name: i + 1 + ").\u200b" + command.data.description,
+            value:
+              " \u200b \u200b" +
+              inlineCode(`/${command.data.name}`) +
+              "\n\n\n\n",
           }))
         );
       await interaction.reply({ embeds: [embed] });
